5d9a95efa0283ebfd4ea1e0e4040ffa8175b2431
rauxsv <rrrauka3@gmail.com>
2023-11-02 02:26:41 +0600

    first commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,6 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    protected $guarded = false;
-
+    protected $fillable = [
+        'userId',
+        'title',
+        'body',
+      ];
 }
+    // Post::create(request([
+    //     'body' => request('body'),
+    //     'title' => request('title'),
+    //     'user_id' => auth()->id()
+    // ]));
